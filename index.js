713f6d60b3f96b012386b3ae731ed49b003fb109
jnf <jeremy.flores@gmail.com>
2016-06-09 08:09:29 -0700

    add a note about what index.js is for and why we're not using it in this project.

---
+++ index.js
@@ -0,0 +1,10 @@
+/*
+  The convention with Node projects is to use `index.js` as the jumping-off
+  point for the application. If we were building a larger, more integrated
+  Scrabble application, we would bring the various modules/objects we made into
+  this file and kick off whatever functions were necessary to run the app.
+
+  We're focusing on learning the module pattern and unit testing right now, so
+  we won't be bringing anything into this file. Next project, we'll make good
+  use of this file.
+*/
