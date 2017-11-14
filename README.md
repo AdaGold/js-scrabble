995fc5cfff05b2ea0dbef807f8dbb166d3aad3ee
Dan Roberts <dan@adadevelopersacademy.org>
2017-11-13 17:02:20 -0800

    Add some tests

--- README.md
+++ README.md
@@ -5,14 +5,31 @@ This is an individual [Stage 1](https://github.com/Ada-Developers-Academy/pedago
 
 ## Project Expectations
 
-From the project root, you should be able to execute your code with `node scrabble.js`.  Each function you create should be a method of the `Scrabble` class.
+From the project root, you should be able to execute your code with `node scrabble.js`.  Each function you create should be a method of the `Scrabble` object.
 
-## Baseline Requirements
-Review the requirements and come up with a "plan of action" for how you want to approach this problem.
+### Project contents
 
-The baseline repository contains an empty node project which contains the starter structure to get you started.
+This repository contains an empty node project which contains some structure to get you started. To finish setting up, go to the command line and run:
 
-#### Score chart
+```
+npm install
+```
+
+### Testing
+
+This project contains a set of tests written using the Jasmine test framework. Use these tests to enable a test-driven development workflow. To run the tests use the command:
+
+```
+npm test
+```
+
+### Error Handling
+
+Here is one question that's always worth asking: what should your code do when given invalid input?
+
+There are many possible answers to this question, but according to the tests these methods should throw an exception. The syntax for doing so in JavaScript is a little different than in Ruby but the high level philosophy is the same. Go do some research and figure it out!
+
+### Score chart
 
 |Letter                        | Value|
 |:----------------------------:|:----:|
@@ -28,28 +45,27 @@ The baseline repository contains an empty node project which contains the starte
 ## Wave 1
 
 ### Primary Requirements
-Create the following methods within the `Scrabble` class.  
+Create the following methods within the `Scrabble` class.
 - `score(word)`: returns the total score value for the given word. The word is input as a string (case insensitive). The chart below shows the point value for a given letter.
 - `highestScoreFrom(arrayOfWords)`: returns **the word in the array with the highest score**.
     - Note that it’s better to use fewer tiles, so if the top score is tied between multiple words, pick the one with the fewest letters.
     - Note that there is a bonus (50 points) for using all seven letters. If the top score is tied between multiple words and one used all seven letters, choose the one with seven letters over the one with fewer tiles.
     - If the there are multiple words that are the same score and same length, pick the first one in supplied list.
 
-You should write your own code using the Scrabble class and test the methods manually to ensure they work properly.  
+You should write your code in the Scrabble object. In addition to the provided tests, you should test the methods manually to ensure they work properly.
 
 You can run your `scrabble.js` file by typing:
 
 ```bash
-$  node scrabble.js
+$ node scrabble.js
 ```
-
 
 ## Wave 2
 
 ### Primary Requirements
-Create a new `Player` class. The class should have the following methods:
+Create a new `Player` class within the `Scrabble` object. The class should have the following methods:
 
-- Constructor: Called when you use `new Player(name)`, sets up an instance with the instance variable `name` assigned
+- Constructor: Called when you use `new Scrabble.Player(name)`, sets up an instance with the instance variable `name` assigned
 - `name`: property which returns the value of the player's name
 - `plays`: property which returns an Array of the words played by the player
 - `play(word)`: method which adds the input word to the `plays` Array
@@ -60,7 +76,7 @@ Create a new `Player` class. The class should have the following methods:
 - `highestWordScore()`: method which returns the `highestScoringWord` score
 
 ### Optional Enhancements
-- Create a `TileBag` object that includes functionality that allows us to drawTiles and determine the remaining tiles.
+- Create a `Scrabble.TileBag` object that includes functionality that allows us to drawTiles and determine the remaining tiles.
 Beginning Tile Quantities:
 
 | Tiles per letter | Letters                   |
@@ -76,4 +92,4 @@ Beginning Tile Quantities:
 
 - Create a `Dictionary` class that includes a method for searching a list of words to determine if a given word is a valid word.
 - Create a `Board` class that has a matrix (array of arrays) of tile places. Check if a word can be played on a given tile place in a certain direction.
-- Examine the Scrabble tests we wrote in our Ruby project, and see if you can write some code which tests your JavaScript project's functionality.  
+- Using the existing tests as an example, write some tests for your optionals!
---
+++ spec/scrabble_spec.js
@@ -0,0 +1,216 @@
+const Scrabble = require('../scrabble');
+
+describe('score', function() {
+  it ('correctly scores simple words', function() {
+    expect(Scrabble.score('dog')).toBe(5);
+    expect(Scrabble.score('cat')).toBe(5);
+    expect(Scrabble.score('pig')).toBe(6);
+  });
+
+  it ('adds 50 points for a 7-letter word', function() {
+    expect(Scrabble.score('academy')).toBe(65);
+  });
+
+  it ('throws on bad characters', function() {
+    expect(function () {
+      Scrabble.score('char^');
+    }).toThrow();
+  });
+
+  it ('handles all upper- and lower-case letters', function() {
+    expect(Scrabble.score('dog')).toBe(5);
+    expect(Scrabble.score('DOG')).toBe(5);
+    expect(Scrabble.score('DoG')).toBe(5);
+  });
+
+  it ("doesn't allow words > 7 letters", function() {
+    expect(function() { Scrabble.score('abcdefgh'); }).toThrow();
+  });
+
+  it ("doesn't allow empty words", function() {
+    expect(function() { Scrabble.score(''); }).toThrow();
+  });
+});
+
+describe('highestScoreFrom', function() {
+  it ('throws if no words were passed', function() {
+    expect(function() { Scrabble.highestScoreFrom([]); }).toThrow();
+    expect(function() { Scrabble.highestScoreFrom('not array'); }).toThrow();
+  });
+
+  it ('returns the only word in a length-1 array', function() {
+    expect(Scrabble.highestScoreFrom(['dog'])).toBe('dog');
+  });
+
+  it ('returns the highest word if there are two words', function() {
+    // Check score assumptions
+    expect(Scrabble.score('dog')).toBe(5);
+    expect(Scrabble.score('pig')).toBe(6);
+
+    // Test the functionality
+    expect(Scrabble.highestScoreFrom(['dog', 'pig'])).toBe('pig');
+    expect(Scrabble.highestScoreFrom(['pig', 'dog'])).toBe('pig');
+  });
+
+  it ('if tied, prefer a word with 7 letters', function() {
+    loser = 'zzzzzz';
+    winner = 'iiiiddd';
+
+    // Check score assumptions
+    expect(Scrabble.score(loser)).toBe(60);
+    expect(Scrabble.score(winner)).toBe(60);
+
+    // Test functionality
+    expect(Scrabble.highestScoreFrom([loser, winner])).toBe(winner);
+    expect(Scrabble.highestScoreFrom([winner, loser])).toBe(winner);
+  });
+
+  it ('if tied and no word has 7 letters, prefers the word with fewer letters', function() {
+    // Check score assumptions
+    expect(Scrabble.score('dog')).toBe(5);
+    expect(Scrabble.score('goat')).toBe(5);
+
+    // Test functionality
+    expect(Scrabble.highestScoreFrom(['dog', 'goat'])).toBe('dog');
+    expect(Scrabble.highestScoreFrom(['goat', 'dog'])).toBe('dog');
+  });
+
+  it ('returns the first word of a tie with same letter count', function() {
+    // Check score assumptions
+    expect(Scrabble.score('i')).toBe(1);
+    expect(Scrabble.score('dog')).toBe(5);
+    expect(Scrabble.score('cat')).toBe(5);
+
+    // Test the functionality
+    expect(Scrabble.highestScoreFrom(['dog', 'dog'])).toBe('dog');
+    expect(Scrabble.highestScoreFrom(['dog', 'cat'])).toBe('dog');
+    expect(Scrabble.highestScoreFrom(['cat', 'dog'])).toBe('cat');
+    expect(Scrabble.highestScoreFrom(['i', 'dog', 'cat'])).toBe('dog');
+  });
+});
+
+describe('Player', function() {
+  describe('Constructor', function() {
+    it('Creates a new player', function() {
+      let name = 'test name';
+      let player = new Scrabble.Player(name);
+      expect(player.name).toBe(name);
+    });
+
+    it('Requires a name', function() {
+      expect(function() { new Scrabble.Player(); }).toThrow();
+    });
+  });
+
+  describe ('play', function() {
+    it ('Records the played word', function() {
+      let word = 'dog';
+      let player = new Scrabble.Player('test player');
+      expect(player.plays.length).toBe(0);
+
+      expect(player.play(word)).toBeTruthy();
+
+      expect(player.plays.length).toBe(1);
+      expect(player.plays[0]).toBe(word);
+    });
+
+    it ('Requires a real word', function() {
+      let player = new Scrabble.Player('test player');
+      expect(player.plays.length).toBe(0);
+
+      expect(function() { player.play(); }).toThrow();
+      expect(player.plays.length).toBe(0);
+
+      expect(function() { player.play(44); }).toThrow();
+      expect(player.plays.length).toBe(0);
+    });
+
+    it ("Returns false and doesn't update plays if the player has already won", function() {
+      let player = new Scrabble.Player('test player');
+      expect(player.play('zzzzzzz')).toBeTruthy(); // +120 pts
+      expect(player.plays.length).toBe(1);
+      expect(player.hasWon()).toBeTruthy();
+
+      expect(player.play('dog')).toBe(false);
+      expect(player.plays.length).toBe(1);
+    });
+  });
+
+  describe ('totalScore', function() {
+    it ("Is zero if the player hasn't played anything", function() {
+      let player = new Scrabble.Player('test player');
+      expect(player.totalScore()).toBe(0);
+    });
+
+    it ("Is updated by play", function() {
+      let player = new Scrabble.Player('test player');
+      expect(player.totalScore()).toBe(0);
+      player.play('dog');
+      expect(player.totalScore()).toBe(5);
+      player.play('cat');
+      expect(player.totalScore()).toBe(10);
+      player.play('goat');
+      expect(player.totalScore()).toBe(15);
+    });
+  });
+
+  describe('hasWon', function() {
+    it('returns false when score < 100', function() {
+      let player = new Scrabble.Player('test player');
+      expect(player.totalScore()).toBe(0);
+      expect(player.hasWon()).toBe(false);
+
+      player.play('zzzzz'); // Score += 50
+      expect(player.totalScore()).toBe(50);
+      expect(player.hasWon()).toBe(false);
+
+      player.play('zzzzji'); // Score += 49
+      expect(player.totalScore()).toBe(99);
+      expect(player.hasWon()).toBe(false);
+    });
+
+    it('returns true when score == 100', function() {
+      let player = new Scrabble.Player('test player');
+      player.play('zzzzbbf'); // Score += 100
+      expect(player.totalScore()).toBe(100);
+      expect(player.hasWon()).toBe(true);
+    });
+
+    it('returns true when score > 100', function() {
+      let player = new Scrabble.Player('test player');
+      player.play('zzzzzzz'); // Score += 120
+      expect(player.totalScore()).toBe(120);
+      expect(player.hasWon()).toBe(true);
+    });
+  });
+
+  describe('highestScoringWord', function() {
+    // Tie-breaking logic is already described in the tests
+    // for highestWordFrom, so we will not repeat it here.
+    it('returns the highest scoring word played', function() {
+      let player = new Scrabble.Player('test player');
+      player.play('cat');
+      player.play('zzzz');
+      expect(player.highestScoringWord()).toBe('zzzz');
+    });
+
+    it('throws an error if no words have been played', function() {
+      let player = new Scrabble.Player('test player');
+      expect(() => { player.highestScoringWord() }).toThrow();
+    });
+  });
+
+  describe('highestWordScore', function() {
+    it('returns the highest scoring word played', function() {
+      let player = new Scrabble.Player('test player');
+      player.play('cat');
+      player.play('zzzz');
+      expect(player.highestWordScore()).toBe(40);
+    });
+
+    it('throws an error if no words have been played', function() {
+      let player = new Scrabble.Player('test player');
+      expect(() => { player.highestWordScore() }).toThrow();
+    });
+  });
+});
